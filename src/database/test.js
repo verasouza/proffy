aa791ea71fc8d77cc76667289dfaa0f6474634f1
verasouza <vera.al27@gmail.com>
2020-08-07 20:11:41 -0300

    aula final - integração com banco de dados(sqllite)

---
+++ src/database/test.js
@@ -0,0 +1,52 @@
+const database = require('./db')
+const createProffy = require('./createProffy');
+
+database.then(async(db)=>{
+    //inserir dados
+    proffyValue = {
+        name:"Mayk Brito",
+        avatar:"https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "9887855454",
+        bio:"Instrutor de Eduacação Física",
+        
+    }
+    classValue = {
+        subject: 4,
+        cost: "20",
+        
+    }
+
+    classScheduleValues = [
+        {
+        weekday: 0,
+        time_from: 54000,
+        time_to: 60000
+        },
+        {
+            weekday: 2,
+            time_from: 66000,
+            time_to: 72000
+            }
+    ]
+
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    console.log(selectedProffys)
+
+    const selectedClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+    `)
+    console.log(selectedClassesAndProffys)
+
+    const selectedClassesSchedule = await db.all(`
+        SELECT classes_schedule.*
+        FROM classes_schedule
+        WHERE classes_schedule.class_id = 1
+         
+    `)
+
+    console.log(selectedClassesSchedule)
+})
